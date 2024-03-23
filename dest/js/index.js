7d597860ab627682e28e9653a8017251cd0b79f3
Melvin Bubini <melvin.bubini@gmail.com>
2024-03-23 14:43:22 +0100

    local storage klar, redigering kvar

--- dest/js/index.js
+++ dest/js/index.js
@@ -1,26 +1,48 @@
 "use strict";
-// // elements
-// let form: HTMLFormElement | null = document.getElementById("search-form") as HTMLFormElement;
-// let inputValue: string | null = (document.getElementById("search-input") as HTMLInputElement)?.value ?? null;
-// let subBtn: HTMLButtonElement | null = document.getElementById("subBtn") as HTMLButtonElement | null;
-// if (subBtn) {
-//     // Lägg till händelsehanteraren
-//     subBtn.addEventListener("click", addCourse);
-// }
+//funktion som hämtar kurser från local storage
+function getCourses() {
+    const courseJSON = localStorage.getItem("courses");
+    return courseJSON ? JSON.parse(courseJSON) : [];
+}
+//funktion som sparar kurser till localstorage
+function saveCourses(courses) {
+    localStorage.setItem("courses", JSON.stringify(courses));
+}
+//när sidan laddas skrivs sparade kurser ut
+window.addEventListener("load", () => {
+    const courses = getCourses();
+    courses.forEach(course => {
+        addCourse(course);
+    });
+});
+//rensar listan på sidan och localstorage vid klick på knappen "Ta bort alla kurser"
+const courseDetailsUl = document.getElementById("courseDetailsUl");
+const deleteBtn = document.getElementById("deleteBtn");
+if (deleteBtn) {
+    deleteBtn.addEventListener("click", () => {
+        if (courseDetailsUl) {
+            courseDetailsUl.innerHTML = "";
+        }
+        saveCourses([]); // rensar localStorage        
+    });
+}
 // lägger till en kurs
 function addCourse(courseinfo) {
     const courseDetailsUl = document.getElementById("courseDetailsUl");
     if (courseDetailsUl) {
-        const link = document.createElement('a');
-        link.href = "https://www.miun.se/utbildning/program/webbutveckling/";
-        link.textContent = "Kursplanen";
+        // const link = document.createElement('a');
+        // link.href = "https://www.miun.se/utbildning/program/webbutveckling/";
+        // link.textContent = "Kursplanen";
         const liEl = document.createElement("li");
-        liEl.textContent = `${courseinfo.code}, ${courseinfo.name}, ${courseinfo.progression}`;
-        courseDetailsUl.appendChild(liEl);
-        liEl.appendChild(link);
+        liEl.innerHTML = `${courseinfo.code}, ${courseinfo.name}, ${courseinfo.progression}, <a class="courseplan" href="${courseinfo.syllabus}">${courseinfo.syllabus}</a>`;
+        courseDetailsUl.appendChild(liEl); // Lägg till det nya <li>-elementet till <ul>
     }
 }
 ;
+// Funktion för att kontrollera om en kurskod redan existerar i listan
+function isCourseCodeUnique(courseCode, courseList) {
+    return !courseList.some(course => course.code === courseCode);
+}
 // Hämta DOM-element för formulär och användardetaljer
 const course_form = document.getElementById("course_form");
 // Lägg till händelselyssnare på formuläret
@@ -30,7 +52,7 @@ course_form.addEventListener("submit", (event) => {
     const codeInput = document.getElementById("input_code");
     const nameInput = document.getElementById("input_name");
     const progressionInput = document.querySelectorAll('.radio-input');
-    //   const syllabusInput = document.getElementById("phoneNumber") as HTMLInputElement;
+    const syllabusInput = document.getElementById("input_url");
     // Loopa igenom alla radioknappar för att hitta den valda knappen
     let selectedValue;
     for (let i = 0; i < progressionInput.length; i++) {
@@ -49,8 +71,22 @@ course_form.addEventListener("submit", (event) => {
         code: codeInput.value,
         name: nameInput.value,
         progression: selectedValue !== null && selectedValue !== void 0 ? selectedValue : "",
-        // syllabus: syllabusInput.value
+        syllabus: syllabusInput.value
     };
+    //kontrollerar att kurskoden är unik innan kursen läggs till på listan
+    const courses = getCourses();
+    if (!isCourseCodeUnique(newCourse.code, courses)) {
+        alert("Kurskoden måste vara unik");
+        return;
+    }
+    saveCourseToLocalStorage(newCourse); //anropar funktionen för att spara kursen i localstorage
+    course_form.reset(); //nollställer formuläret
     // Använd printUserDetails för att skriva ut användardetaljer
     addCourse(newCourse);
 });
+// Funktion för att spara kursen i localStorage
+function saveCourseToLocalStorage(course) {
+    const courses = getCourses();
+    courses.push(course);
+    saveCourses(courses);
+}
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -2,24 +2,63 @@ interface Courseinfo {
     code: string;
     name: string;
     progression: string;
-    // syllabus  : string;
+    syllabus: string;
+}
+
+//funktion som hämtar kurser från local storage
+function getCourses(): Courseinfo[] {
+    const courseJSON: string | null = localStorage.getItem("courses");
+    return courseJSON ? JSON.parse(courseJSON) : [];
+}
+
+//funktion som sparar kurser till localstorage
+function saveCourses(courses: Courseinfo[]): void {
+    localStorage.setItem("courses", JSON.stringify(courses));
+}
+
+
+//när sidan laddas skrivs sparade kurser ut
+window.addEventListener("load", () => {
+    const courses: Courseinfo[] = getCourses();
+    courses.forEach(course => {
+        addCourse(course);
+    });
+});
+
+
+
+//rensar listan på sidan och localstorage vid klick på knappen "Ta bort alla kurser"
+const courseDetailsUl = document.getElementById("courseDetailsUl");
+const deleteBtn: HTMLButtonElement | null = document.getElementById("deleteBtn") as HTMLButtonElement | null;
+if (deleteBtn) {
+    deleteBtn.addEventListener("click", () => {
+        if (courseDetailsUl) {
+            courseDetailsUl.innerHTML = "";
+        }
+        saveCourses([]); // rensar localStorage        
+    });
 }
 
 // lägger till en kurs
 function addCourse(courseinfo: Courseinfo): void {
     const courseDetailsUl = document.getElementById("courseDetailsUl");
     if (courseDetailsUl) {
-        const link = document.createElement('a');
-        link.href = "https://www.miun.se/utbildning/program/webbutveckling/";
-        link.textContent = "Kursplanen";
+        // const link = document.createElement('a');
+        // link.href = "https://www.miun.se/utbildning/program/webbutveckling/";
+        // link.textContent = "Kursplanen";
 
         const liEl = document.createElement("li");
-        liEl.textContent = `${courseinfo.code}, ${courseinfo.name}, ${courseinfo.progression}, `;
-
-        courseDetailsUl.appendChild(liEl);
-        liEl.appendChild(link);
+        liEl.innerHTML = `${courseinfo.code}, ${courseinfo.name}, ${courseinfo.progression}, <a class="courseplan" href="${courseinfo.syllabus}">${courseinfo.syllabus}</a>`;
+        courseDetailsUl.appendChild(liEl); // Lägg till det nya <li>-elementet till <ul>
     }
 };
+
+// Funktion för att kontrollera om en kurskod redan existerar i listan
+function isCourseCodeUnique(courseCode: string, courseList: Courseinfo[]): boolean {
+    return !courseList.some(course => course.code === courseCode);
+}
+
+
 
 // Hämta DOM-element för formulär och användardetaljer
 const course_form = document.getElementById("course_form") as HTMLFormElement;
@@ -32,7 +71,7 @@ course_form.addEventListener("submit", (event) => {
     const codeInput = document.getElementById("input_code") as HTMLInputElement;
     const nameInput = document.getElementById("input_name") as HTMLInputElement;
     const progressionInput = document.querySelectorAll<HTMLInputElement>('.radio-input');
-    //   const syllabusInput = document.getElementById("phoneNumber") as HTMLInputElement;
+    const syllabusInput = document.getElementById("input_url") as HTMLInputElement;
 
     // Loopa igenom alla radioknappar för att hitta den valda knappen
     let selectedValue: string | undefined;
@@ -55,9 +94,27 @@ course_form.addEventListener("submit", (event) => {
         code: codeInput.value,
         name: nameInput.value,
         progression: selectedValue ?? "",
-        // syllabus: syllabusInput.value
+        syllabus: syllabusInput.value
     };
+
+    //kontrollerar att kurskoden är unik innan kursen läggs till på listan
+    const courses: Courseinfo[] = getCourses();
+    if (!isCourseCodeUnique(newCourse.code, courses)) {
+        alert("Kurskoden måste vara unik");
+        return;
+    }
+
+    saveCourseToLocalStorage(newCourse);//anropar funktionen för att spara kursen i localstorage
+    course_form.reset();//nollställer formuläret
 
     // Använd printUserDetails för att skriva ut användardetaljer
     addCourse(newCourse);
 });
+
+
+// Funktion för att spara kursen i localStorage
+function saveCourseToLocalStorage(course: Courseinfo): void {
+    const courses: Courseinfo[] = getCourses();
+    courses.push(course);
+    saveCourses(courses);
+}
